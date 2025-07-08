7fe6dfb2e6eda5e4eefffa4c07faabd1cac966e4
anusha <anumadhu.mn@gmail.com>
2025-07-07 22:50:22 -0500

    dropping table table

---
+++ droptable.sql
@@ -0,0 +1,11 @@
+DROP DATABASE practicedb;
+CREATE TABLE Employees_address (
+  ID INT PRIMARY KEY,
+  add1 VARCHAR(100),
+  streetno INT,
+  area VARCHAR(50)
+);
+
+CREATE DATABASE CompanyDB;
+
+drop table Employees_address;
